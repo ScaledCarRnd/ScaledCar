a58d9fc0867e62092f24d389e747da3be73949c0
CoreyJohnKight <coreyk@windowslive.com>
2024-03-14 13:30:44 +1300

    Added Jetracer files so that modifications can be tracked

---
+++ jetracer_ros/scripts/mod2_nav.py
@@ -0,0 +1,15 @@
+import rospy
+from move_base_msgs.msg import *
+from actionlib_msgs.msg import GoalID
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+from geometry_msgs.msg import PointStamped, PoseStamped, PoseWithCovarianceStamped
+from std_msgs.msg import String
+
+class Mod2_Navigation:
+    def __init__(self):
+        print('test')
+
+if __name__ == '__main__':
+    Mod2_Navigation()
+
