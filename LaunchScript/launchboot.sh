4c77490208c3b3ff60ad82c4653af27a3eeb5461
NathanDomFlanagan <jaypedesh7@gmail.com>
2024-04-17 15:19:22 +1200

    Updated launcboot.sh with better naming and adding launch files

--- LaunchScript/launchboot.sh
+++ LaunchScript/launchboot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Paths and commands
+CAMNAV_LAUNCH="jetracer run_camnav.launch"
+NAVMAP_LAUNCH="jetracer run_navmap.launch"
+HELLO_WORLD_PY="/home/jetson/Desktop/LaunchScript/hello_world.py"
+
 # Function to display menu
 display_menu() {
     clear
@@ -8,22 +13,22 @@ display_menu() {
     echo -e " \e[1m\e[32mROS Launch File Selector\e[0m"  # Change text color to green and make it bold
     echo " ------------------------"
     echo
-    echo "  1. Launch File 1"
-    echo "  2. Launch File 2"
+    echo "  1. Launch Camera and Navigation"
+    echo "  2. Launch Slam and RVIZ"
     echo "  3. Launch demo Python Script"
     echo "  4. Quit"
     echo
 }
-# For this display_menu function I guess we could launch important automonous launch files and visual display stuff individualy?
+# For this display_menu function I guess we could launch important automonous launch files and visual display stuff individually?
 
 
 # Function to run selected option
 run_option() {
     case $1 in
         # 1) roslaunch package_name launch_file_1.launch;;
-	1) roslaunch jetracer run_camnav.launch;;
-        2) roslaunch package_name launch_file_2.launch;;
-        3) python3 /home/jetson/Desktop/LaunchScript/hello_world.py;;
+	    1) roslaunch $CAMNAV_LAUNCH;;
+        2) roslaunch $NAVMAP_LAUNCH;;
+        3) python3 $HELLO_WORLD_PY;;
         4) echo " Exiting..."; exit;;
         *) echo " Invalid option";;
     esac
@@ -34,11 +39,21 @@ main() {
     while true; do
         display_menu
         read -p " Enter your choice: " choice
-        run_option $choice
-        read -p " Press Enter to continue..."
+        # run_option $choice
+        # read -p " Press Enter to continue..."
+        if [[ ! $choice =~ ^[1-4]$ ]]; then
+            echo "Invalid option. Please enter a number between 1 and 4."
+        else
+            run_option $choice
+            read -rp "Press Enter to continue or type 'x' to quit: " input
+            # The input is case-sensitive
+            if [ "${input,,}" = "x" ]; then
+                echo "Exiting..."
+                exit
+            fi
+        fi
     done
 }
 
 # Call the main function
 main
-
