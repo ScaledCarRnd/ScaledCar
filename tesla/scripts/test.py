c5f6931e3a80d9c6436d83cc6ece95eaac43eeac
SwagTemplar <38060224+SwagTemplar@users.noreply.github.com>
2024-05-07 16:29:19 +1200

    Added A real life test file

--- tesla/scripts/test.py
+++ tesla/scripts/test.py
@@ -1,7 +1,7 @@
 import unittest
 from camfunctions import *
 
-class TestWidthFinder(unittest.TestCase):
+class TestMetricFinder(unittest.TestCase):
 
     def setUp(self):
         self.widtherr = 'Expected width not returned.'
@@ -24,7 +24,6 @@ class TestWidthFinder(unittest.TestCase):
         self.assertEqual(widthFinder(1156, 1280), 4, self.widtherr)
         self.assertEqual(widthFinder(1200, 1250), 4, self.widtherr)
 
-
     # Test for when only bl is below 246 pixels
     def test_only_bl_below_246(self):
         self.assertEqual(widthFinder(200, 705), 20, self.widtherr)
@@ -43,6 +42,31 @@ class TestWidthFinder(unittest.TestCase):
     def test_bl_above_705_below_1156_br_above_1155(self):
         self.assertEqual(widthFinder(800,1200), 14, self.widtherr)
         self.assertEqual(widthFinder(706, 1156), 14, self.widtherr)
+
+
+    # Test for distances
+    # Test cases for distanceFinder function
+    def test_distanceFinder(self):
+        # Test case 1: bp > 392
+        self.assertEqual(distanceFinder(400), 5,"Test case 1 failed")
+        
+        # Test case 2: 207 < bp <= 392
+        self.assertEqual(distanceFinder(300), 10,"Test case 2 failed")
+
+        # Test case 3: 121 < bp <= 207
+        self.assertEqual(distanceFinder(150), 20,"Test case 3 failed")
+        
+        # Test case 4: 72 < bp <= 121
+        self.assertEqual(distanceFinder(100), 30,"Test case 4 failed")
+        
+        # Test case 5: 40 < bp <= 72
+        self.assertEqual(distanceFinder(50), 40,"Test case 5 failed")
+        
+        # Test case 6: 22 < bp <= 40
+        self.assertEqual(distanceFinder(30), 50,"Test case 6 failed")
+        
+        # Test case 7: bp <= 22
+        self.assertEqual(distanceFinder(10), 60,"Test case 7 failed")
         
 
 if __name__ == '__main__':
